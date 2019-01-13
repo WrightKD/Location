f1dbca61d9502fc818ad56a0c0506a467fe0f1ef
WrightKD <kennethdavewright18@gmail.com>
2019-01-13 13:00:04 +0200

    Added basic usage of PAPI API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MapService } from './map.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'location';
+   
+  latitude: string ='';
+  longitude: string ='';
+
+  location: Object;
+
+  constructor(private map: MapService){}
+
+  ngOnInit(){
+    this.map.getLocation().subscribe(data =>{
+      console.log(data);
+      this.latitude = data.latitude;
+      this.longitude = data.longitude;
+    })
+  }
 }
--- src/app/map.service.ts
+++ src/app/map.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+interface data{
+  
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getLocation(){
+    return this.http.get('https://ipapi.co/json/')
+  }
 }
